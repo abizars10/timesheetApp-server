16d507a1beef32fcd20d50ac69dbc9d408b9027c
Abizar Ar Rifa'i <abizararrifai@gmail.com>
2024-08-18 23:11:08 +0700

    updated

--- user.js
+++ user.js
@@ -3,7 +3,8 @@ const { Client } = require("pg");
 
 // konfigurasi koneksi ke database menggunakan DATABASE_URL
 const client = new Client({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.PGSSLMODE === "require" ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = client;
